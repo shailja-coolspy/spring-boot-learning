b0ef9cd965eac627403850d01a0dc803b1f6abaf
shailja-coolspy <agarwalshailja1@gmail.com>
2024-05-15 18:25:02 +0530

    remove not needed file

---
+++ 21-springBoot-aopDemo/src/test/java/com/example/springBoot/aopDemo/AopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBoot.aopDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
